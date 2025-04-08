cceb399e4971a4ddaa52cf9382b389ec0227c705
Lane Simpson <lanesimpson14@gmail.com>
2025-04-08 12:58:41 -0400

    Added game addition button; still wont update db...

--- game-tracker-client/src/app/app.routes.ts
+++ game-tracker-client/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { GameListComponent } from './components/game-list/game-list.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'games', pathMatch: 'full' }, // Redirect base URL to /games
-  { path: 'games', component: GameListComponent }         // Loads GameListComponent when visiting /games
+  { path: '', redirectTo: 'games', pathMatch: 'full' },
+  { path: 'games', component: GameListComponent },
+  { path: 'games/create', component: GameFormComponent },
 ];
