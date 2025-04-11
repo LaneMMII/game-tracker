88de366333e8426655257560251fc3f32845422b
Lane Simpson <lanesimpson14@gmail.com>
2025-04-11 13:17:14 -0400

    Added Game Detail section

--- game-tracker-client/src/app/app.routes.ts
+++ game-tracker-client/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { GameListComponent } from './components/game-list/game-list.component';
 import { GameFormComponent } from './components/game-form/game-form.component';
 import { PlatformFormComponent } from './components/platform-form/platform-form.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
 
 export const routes: Routes = [
   { path: 'games', component: GameListComponent },
   { path: 'games/create', component: GameFormComponent },
+  { path: 'games/:id', component: GameDetailComponent },
   { path: 'platforms/create', component: PlatformFormComponent },
   { path: '', redirectTo: '/games', pathMatch: 'full' }
 ];
